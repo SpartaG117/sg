a8cfe9aca6110bbaf211f3347370de70d9543796
SpartaG117 <444219104@qq.com>
2018-12-29 19:00:45 +0800

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,88 +1,93 @@
 import torch
 import torch.nn.functional as F
 
-def get_sample(model, batch_size, seq_len, src, x=None):
-    """
-    Todo whether the seq_len includes the start token or end token
-    Todo padding token may be sampled
-    :param model:
-    :param batch_size:
-    :param seq_len:
-    :param src:
-    :param x:
-    :return:
-    """
-    # Todo end token
-    sample = []
+def get_sample(generator, batch_size, max_seq_len, src, tgt=None):
 
     # sample from zero
-    if x is None:
+    if tgt is None:
 
-        pad_mask = torch.zeros([batch_size]).byte()
-        # Todo
-        pad_tokens = torch.Tensor([2 for _ in range(batch_size)])
+        tgt_seq_pad = torch.zeros([batch_size, max_seq_len - 1]).long()
+        tgt_seq = torch.Tensor([9488] * batch_size).long().unsqueeze(1)
+        tgt_seq = torch.cat([tgt_seq, tgt_seq_pad], dim=1).contiguous().cuda()
 
-        # Todo should be the start token
-        x = torch.zeros([batch_size, 1]).long()
-        x = x.cuda()
-        x_pos = torch.Tensor([batch_size, 1]).long()
-        x_pos = x_pos.cuda()
-        for i in range(seq_len):
+        tgt_pos_pad = torch.zeros([batch_size, max_seq_len - 1]).long()
+        tgt_pos = torch.ones([batch_size, 1]).long()
+        tgt_pos = torch.cat([tgt_pos, tgt_pos_pad], dim=1).contiguous().cuda()
+
+        zero_mask = torch.zeros([batch_size]).byte().cuda()
+
+        end_tokens = torch.Tensor([9489 for _ in range(batch_size)]).long().cuda()
+        pad_tokens = torch.Tensor([0 for _ in range(batch_size)]).long().cuda()
+        start_tokens = torch.Tensor([9488 for _ in range(batch_size)]).long().cuda()
+
+        for i in range(max_seq_len-1):
             # pred: batch_size x num_word
-            pred = model(src, (x, x_pos)).view(batch_size, i+1, -1)[:, -1, :]
+            pred = generator(src, (tgt_seq, tgt_pos)).view(batch_size, max_seq_len, -1)[:, i, :]
             pred_prob = F.softmax(pred, dim=1)
-            # s_word: batch_size x 1
-            s_word = pred_prob.multinomial(1)
-            s_word = s_word.masked_fill(pad_mask, 0)
-            sample.append(s_word)
-            x = torch.cat([x, s_word], dim=1)
-            # make the right padding position
-            cur_pos = torch.Tensor([i+1 for _ in range(batch_size)]).long()
-            cur_pos = cur_pos.masked_fill(pad_mask, 0)
-            x_pos = torch.cat([x_pos, cur_pos], dim=1)
-            # TODO end_token=2  padding_token=0 start_token=1 ?
-            pad_mask = torch.le(s_word, pad_tokens)
+            # pred_word: batch_size x 1
+            pred_word = pred_prob.multinomial(1).view(-1)
+            pred_word = pred_word.masked_fill(zero_mask, 0)
 
-        output = torch.cat(sample, dim=1)
-        x_pos = (x_pos - 1)[:, 1:]
-        pad_mask = x_pos.eq(1)
-        x_pos.masked_fill(pad_mask, 0)
-        return output, x_pos
+            pred_pos = (tgt_pos[:, i] + 1).masked_fill(zero_mask, 0)
+            tgt_pos[:, i + 1] = pred_pos
+
+            end_mask = pred_word.eq(end_tokens)
+            start_mask = pred_word.eq(start_tokens)
+            pad_mask = pred_word.eq(pad_tokens)
+            zero_mask = (end_mask + start_mask + pad_mask).ge(1)
+
+            tgt_seq[:, i + 1] = pred_word
+
+        return tgt_seq, tgt_pos
 
     # sample from given sequence
     else:
 
-        x, x_pos = x
-        sample.append(x)
-        num_given = x.size(1)
-        x_pos = x_pos.cuda()
+        tgt_seq, tgt_pos = tgt
+        num_given = tgt_seq.size(1)
+        tgt_pos = tgt_pos.cuda()
 
-        # Todo
-        pad_tokens = torch.Tensor([2 for _ in range(batch_size)])
-        pad_mask = x[:, -1].le(pad_tokens)
 
-        for i in range(num_given, seq_len):
-            # pred: batch_size x num_dic
-            pred = model(src, (x, x_pos)).view(batch_size, i+1, -1)[:, -1, :]
+        end_tokens = torch.Tensor([9489 for _ in range(batch_size)]).long().cuda()
+        pad_tokens = torch.Tensor([0 for _ in range(batch_size)]).long().cuda()
+        start_tokens = torch.Tensor([9488 for _ in range(batch_size)]).long().cuda()
+
+        end_mask = tgt_seq[:, -1].eq(end_tokens)
+        pad_mask = tgt_seq[:, -1].eq(pad_tokens)
+        zero_mask = (end_mask + pad_mask).ge(1)
+
+        for i in range(num_given - 1, max_seq_len):
+
+            if zero_mask.sum().item() == len(zero_mask):
+                if (max_seq_len + 1 - (i + 1)) > 0:
+
+                    tgt_seq_pad = torch.zeros([batch_size, max_seq_len + 1 - (i + 1)]).long().cuda()
+                    tgt_seq = torch.cat([tgt_seq, tgt_seq_pad], dim=1)
+                    # tgt_seq = torch.cat([tgt_seq, tgt_seq_pad], dim=1)
+                    tgt_pos_pad = torch.zeros([batch_size, max_seq_len + 1 - (i + 1)]).long().cuda()
+                    # tgt_pos = torch.cat([tgt_pos, tgt_pos_pad], dim=1)
+                    tgt_pos = torch.cat([tgt_pos, tgt_pos_pad], dim=1)
+                break
+
+            # pred: batch_size x num_word
+            pred = generator(src, (tgt_seq, tgt_pos)).view(batch_size, i+1, -1)[:, -1, :]
             pred_prob = F.softmax(pred, dim=1)
-            # s_word: batch_size x 1
-            s_word = pred_prob.multinomial(1)
-            s_word = s_word.masked_fill(pad_mask, 0)
-            sample.append(s_word)
-            x = torch.cat([x, s_word], dim=1)
-            # make the right padding position
-            cur_pos = torch.Tensor([i+1 for _ in range(batch_size)]).long()
-            cur_pos = cur_pos.masked_fill(pad_mask, 0)
-            x_pos = torch.cat([x_pos, cur_pos], dim=1)
-            # TODO end_token=2  padding_token=0 start_token=1 ?
-            pad_mask = torch.le(s_word, pad_tokens)
+            # pred_word: batch_size x 1
+            pred_word = pred_prob.multinomial(1).view(-1)
+            pred_word = pred_word.masked_fill(zero_mask, 0)
 
-        output = torch.cat(sample, dim=1)
-        x_pos = (x_pos - 1)[:, 1:]
-        pad_mask = x_pos.eq(1)
-        x_pos.masked_fill(pad_mask, 0)
+            pred_pos = (tgt_pos[:, i] + 1).masked_fill(zero_mask, 0)
+            tgt_pos = torch.cat([tgt_pos, pred_pos.unsqueeze(1)], dim=1)
 
-        return output, x_pos
+            end_mask = pred_word.eq(end_tokens)
+            start_mask = pred_word.eq(start_tokens)
+            pad_mask = pred_word.eq(pad_tokens)
+
+            zero_mask = (end_mask + start_mask + pad_mask).ge(1)
+
+            tgt_seq = torch.cat([tgt_seq, pred_word.unsqueeze(1)], dim=1)
+
+        return tgt_seq, tgt_pos
 
 
 
