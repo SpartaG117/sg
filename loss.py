d59d07bffafd3bb19fb586cc5f7a0a5d1469664d
SpartaG117 <444219104@qq.com>
2018-12-29 19:02:17 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -6,27 +6,24 @@ class PGLoss(nn.Module):
         super(PGLoss, self).__init__()
         self.log_softmax = nn.LogSoftmax(dim=2)
 
-    def forward(self, preds, tgt, reward):
+    def forward(self, preds, label, reward):
         """
-        :param preds: a tensor with shape [batch_size x seq_len x num_word]
-        :param label: a tuple ([batch_size x seq_len], [batch_size x seq_len])
+        :param preds: tensor [batch_size x seq_len x num_word]
+        :param tgt: tensor ([batch_size x seq_len])
         :param reward: a tensor with shape [batch_size x seq_len]
         :return:
         """
-        prob = self.softmax(preds).view(-1, preds.size(2))
-        batch_size = prob.size(0)
-        num_class = prob.size(1)
 
-        tgt, tgt_pos = tgt
-        pad_filter_mask = tgt.view(-1).gt(0)
+        prob = self.log_softmax(preds)
 
         # tgt_mask: tensor [(batch_size x seq_len) x 1]
-        tgt_mask = torch.zeros(batch_size, num_class).cuda().scatter_(1, tgt.view(-1, 1), 1)
+        log_porb = torch.gather(prob, 2, label.unsqueeze(2)).view(-1)
+
+        pad_filter_mask = label.contiguous().view(-1).gt(0)
 
         # loss: tensor [(batch_size x seq_len)]
-        loss = prob.mask_select(tgt_mask).view(-1)
-        loss = loss * (reward.view(-1))
-        loss = loss.mask_select(pad_filter_mask)
+        loss = log_porb * reward
+        loss = loss.masked_select(pad_filter_mask)
         loss = -(loss.mean())
 
         return loss
